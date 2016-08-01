b3324b6f9593b731247ce4eb61ba8bf78540e650
evinoca <evinoca@gmail.com>
2016-08-01 23:26:48 +0800

    Add module for testcases

--- TeSonic/app/models.py
+++ TeSonic/app/models.py
@@ -35,7 +35,18 @@ class User(UserMixin, db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
-
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+
+class TestCase(db.Model):
+    __tablename__='testcases'
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(64))
+
+
+class TestPlan(db.Module):
+    __tablename__='testplans'
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(64))
